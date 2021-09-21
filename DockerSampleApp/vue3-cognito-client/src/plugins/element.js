4727d7437de7ab4e84e1abfaea087a8556cc414f
tos <tos@st-ts.net>
2021-09-21 22:41:30 +0900

    vue3 axios

--- DockerSampleApp/vue3-cognito-client/src/plugins/element.js
+++ DockerSampleApp/vue3-cognito-client/src/plugins/element.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/ja'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/ja'
 
-// 参考: https://element-plus.org/#/jp/component/i18n
-// あってそうだが、上手く日本語になっていない。。。
-Vue.use(Element, { locale })
+export default (app) => {
+  app.use(ElementPlus, { locale })
+}
