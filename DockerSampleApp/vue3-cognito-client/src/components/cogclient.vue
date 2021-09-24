4ba5f91047c5c8686d711f72a5170be52529d164
tos <tos@st-ts.net>
2021-09-24 23:00:44 +0900

    JavaScriptからcognitoを呼び出すように修正

--- DockerSampleApp/vue3-cognito-client/src/components/cogclient.vue
+++ DockerSampleApp/vue3-cognito-client/src/components/cogclient.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
+    <hr />
     REGION {{ REGION }}<br />
     USERPOOLID {{ USERPOOLID }}<br />
     CLIENTID {{ CLIENTID }}<br />
+    <div>ユーザ</div>
+    <el-input placeholder="Please input" v-model="user"></el-input>
+    <div>パスワード</div>
+    <el-input
+      show-password
+      placeholder="Please input"
+      v-model="loginpasswd"
+    ></el-input>
+    <el-button v-on:click="auth_cog()">認証</el-button>
+    <hr />
+    accessToken {{ accessToken }}<br />
+    idToken {{ idToken }}<br />
+    <hr />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser,
+} from "amazon-cognito-identity-js";
+//import * as AWS from 'aws-sdk/global';
 export default {
   name: "cogclient",
   props: {
@@ -18,6 +38,10 @@ export default {
       REGION: "",
       USERPOOLID: "",
       CLIENTID: "",
+      user: "",
+      loginpasswd: "",
+      accessToken: "", // Cognitoのアクセストークン
+      idToken: "", // Cognitoのidトークン
     };
   },
   mounted: function () {
@@ -58,6 +82,75 @@ export default {
             type: "error",
           });
         });
+    },
+    auth_cog: function () {
+      let vm = this;
+      console.log("handleClose");
+
+      // Cognitoで認証する
+      // 参考: https://github.com/aws-amplify/amplify-js/tree/master/packages/amazon-cognito-identity-js
+      var authenticationData = {
+        Username: vm.user,
+        Password: vm.loginpasswd,
+      };
+      var authenticationDetails = new AuthenticationDetails(authenticationData);
+      var poolData = {
+        // ここを書き換える
+        UserPoolId: vm.USERPOOLID, // Your user pool id here
+        ClientId: vm.CLIENTID, // Your client id here
+      };
+      var userPool = new CognitoUserPool(poolData);
+      var userData = {
+        Username: vm.user,
+        Pool: userPool,
+      };
+      var cognitoUser = new CognitoUser(userData);
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+          var accessToken = result.getAccessToken().getJwtToken();
+          // 追加した
+          vm.accessToken = accessToken;
+          // 追加した。アクセストークンとidTokenがある。
+          var idToken = result.getIdToken().getJwtToken();
+          vm.idToken = idToken;
+          vm.$message("アクセストークン取得");
+          console.log(accessToken);
+          vm.loginVisible = false;
+          return; // ここ以降はIDプール用な気がした
+
+          //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+          // ここを書き換える
+          /*
+          AWS.config.region = "us-west-2";
+
+          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            // ここを書き換える
+            IdentityPoolId: "...", // your identity pool id here
+            Logins: {
+              // Change the key below according to the specific region your user pool is in.
+              "cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>": result
+                .getIdToken()
+                .getJwtToken(),
+            },
+          });
+
+          //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+          AWS.config.credentials.refresh((error) => {
+            if (error) {
+              console.error(error);
+            } else {
+              // Instantiate aws sdk service objects now that the credentials have been updated.
+              // example: var s3 = new AWS.S3();
+              console.log("Successfully logged!");
+            }
+          });
+          */
+        },
+
+        onFailure: function (err) {
+          alert(err.message || JSON.stringify(err));
+        },
+      });
     },
   },
 };
