c5fc745fc9ed5897120758bc8024f1d768ebe149
tos <tos@st-ts.net>
2021-09-27 22:58:35 +0900

    FastAPIでJWTをチェック

--- DockerSampleApp/fastapi/main.py
+++ DockerSampleApp/fastapi/main.py
@@ -13,6 +13,11 @@ from jose import JWTError, jwt
 import logging
 import logging.handlers
 
+import time
+import requests
+from jose import jwk, jwt
+from jose.utils import base64url_decode
+
 logging.basicConfig(
     level=logging.INFO, # info異常でないとpythonの標準ライブラリ等のログがDEBUGで出ている
     format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)s: %(message)s'
@@ -48,3 +53,69 @@ async def cog_info():
         "USERPOOLID": os.environ.get('USERPOOLID', "unknown"),
         "CLIENTID": os.environ.get('CLIENTID', "unknown"),
     }
+
+# 参考: https://dev.classmethod.jp/articles/verify_cognit_idtoken_by_apig_custom_auth/
+def is_valid_token(token):
+    headers = jwt.get_unverified_headers(token)
+    kid = headers['kid']
+    REGION=os.environ.get('REGION', "unknown")
+    USERPOOLID=os.environ.get('USERPOOLID', "unknown")
+    CLIENTID=os.environ.get('CLIENTID', "unknown")
+    keys_url = f'https://cognito-idp.{ REGION }.amazonaws.com/{ USERPOOLID }/.well-known/jwks.json'
+    res_cognito = requests.get(keys_url)
+
+    # エンドポイントが見つからなかった場合の処理
+    if res_cognito.status_code != 200:
+        msg = 'Http request to cognito jwks endpoint failed'
+        return {'is_valid_token': False, 'msg': msg, 'claims': None}
+
+    keys = json.loads(res_cognito.text)['keys']
+
+    key_index = -1
+    for i in range(len(keys)):
+        if kid == keys[i]['kid']:
+            key_index = i
+            break
+    # エンドポイントから公開鍵が見つからなかった場合の処理
+    if key_index == -1:
+        msg = 'Public key not found in jwks.json'
+        return {'is_valid_token': False, 'msg': msg, 'claims': None}
+
+    public_key = jwk.construct(keys[key_index])
+
+    message = str(token).rsplit('.', 1)[0].encode('utf-8')
+    encoded_signature = str(token).rsplit('.', 1)[1].encode('utf-8')
+
+    decoded_signature = base64url_decode(encoded_signature)
+
+    # JWTの署名チェックが失敗した場合の処理
+    if not public_key.verify(message, decoded_signature):
+        msg = 'Signature verification failed'
+        return {'is_valid_token': False, 'msg': msg, 'claims': None}
+
+    claims = jwt.get_unverified_claims(token)
+
+    # JWTの有効期限が切れていた場合の処理
+    if time.time() > claims['exp']:
+        msg = 'Token is expired'
+        return {'is_valid_token': False, 'msg': msg, 'claims': None}
+    # audクレームが想定された値でない場合の処理
+    # CognitoのJWTのaudクレームには、認証されたユーザーで使用されるclient_idが含まれる
+    if claims['aud'] not in CLIENTID:
+        msg = 'Token was not issued for this audience'
+        return {'is_valid_token': False, 'msg': msg, 'claims': None}
+
+    return {'is_valid_token': True, 'msg': 'Signature successfully verified', 'claims': claims}
+
+@app.get('/checkjwt')
+def checkjwt(response: Response, imsi:str="", sort:str="desc", to:int=123, limit:int=10, authorization: Optional[str] = Header(None)):
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    if authorization == None:
+        logger.info(f"authorization == None")
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED)
+    ret = is_valid_token(authorization)
+    if ret.get("is_valid_token") != True:
+        # パスワード不一致のため何もしない
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return { "ok": True }
